995dea341332aeaa4dff6729fd28261a9e609334
parentda <danielparent21@gmail.com>
2021-09-16 17:26:55 -0400

    added multiply method

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class Calculator
-  def add(a, b)
-    a + b
+  def add(*nums)
+    nums.sum
+  end
+
+  def multiply(*nums)
+    nums.reduce { |num, product| num * product }
   end
 end
