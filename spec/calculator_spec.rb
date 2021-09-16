2649d3414c9a9a36e497d6d12191f8df7296548b
parentda <danielparent21@gmail.com>
2021-09-16 17:27:24 -0400

    added multiply method tests

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -14,4 +14,16 @@ describe Calculator do
       expect(calculator.add(2, 5, 7)).to eql(14)
     end
   end
+
+  describe '#multiply' do
+    it 'returns the product of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.multiply(5, 2)).to eql(10)
+    end
+
+    it 'returns the product of more than two numbers' do
+      calculator = Calculator.new
+      expect(calculator.multiply(2, 5, 7)).to eql(70)
+    end
+  end
 end
